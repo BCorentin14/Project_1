3403df3d4b6bf91af1d55ef9353947479698cee0
BCorentin14 <100408805+BCorentin14@users.noreply.github.com>
2022-03-07 11:27:28 +0100

    Add files via upload

--- Projet.py
+++ Projet.py
@@ -2,28 +2,35 @@
 price_pop = 5
 # prix des tickets
 price_ticket = 12
-# Demande la somme du porte-feuille
-wallet = int(input("Combien avez vous dans votre porte-feuille ?"))
-print ("Vous avez " + str(wallet) + "€ dans votre porte-feuille")
-# demandez l'age de la personne
-age = int(input("Quel est votre age ?"))
-print ("Vous avez " + str(age) + " ans !")
+try:
+    # Demande la somme du porte-feuille
+    wallet = int(input("Combien avez vous dans votre porte-feuille ?"))
+except:
+    print ("Veuillez entrer un nombre.")
+    wallet = int(input("Combien avez vous dans votre porte-feuille ?"))
+    print ("Vous avez " + str(wallet) + "€ dans votre porte-feuille")
+try:    
+    # demandez l'age de la personne
+    age = int(input("Quel est votre age ?"))
+except:
+    print ("Veuillez entrer un nombre.")
+    age = int(input("Quel est votre age ?"))
+    print ("Vous avez " + str(age) + " ans !")
 
 # Si age personne < 18ans --> 7€
 if age < 18:
     price_ticket = 7
     print ("Le prix de votre ticket est de " + str(price_ticket) + "€")
-# si age personne > 18 --> 12€
-elif age >= 18:
+else:
     print ("Le prix de votre ticket est donc de " + str(price_ticket) + "€") 
 # demander à la personne si elle veut pop corn
 pop_corn = input ("Voulez vous des pops-corns ? (Oui, Non)")
 # si réponse Oui --> affiche le prix pops corns
-if pop_corn == "Oui":
+if pop_corn == "Oui" or "oui":
     print ("Le prix est de " + str(price_pop) + "€")
     print ("Cela vous couteras " + str(price_pop + price_ticket) + "€")
 # Si réponse Non --> n'affiche pas le prix
-elif pop_corn == "Non":
+else:
     print ("Cela vous couteras " + str(price_ticket) + "€")
 
 # demande si la personne veut payer
@@ -33,7 +40,7 @@ total_price = price_pop + price_ticket
 total_ticket = price_ticket
 
 # si la réponse est Oui et qu'il a assez d'argent alors le calcul se fait
-if request == "Oui" and pop_corn == "Oui" and price_ticket + price_pop <= wallet:
+if request == "Oui" or "oui" and pop_corn == "Oui" or "oui" and price_ticket + price_pop <= wallet:
     print ("Il vous resteras " + str(wallet - total_price) + "€ dans votre porte-feuille")
 # Si réponse Oui et que pop corn -> Non et qu'il a assez d'argent alors le calcul se fait
 elif request == "Oui" and pop_corn == "Non" and price_ticket <= wallet:
@@ -42,8 +49,7 @@ elif request == "Oui" and pop_corn == "Non" and price_ticket <= wallet:
 elif request == "Oui" and total_price > wallet and total_ticket > wallet:
     print ("Désoler vous n'avez pas assez d'argent pour payer donc je peut pas vous faire entrer !")
 # si réponse Non -> peut pas rentrer
-elif request == "Non":
+else:
     print ("Vous ne pouvez donc pas rentrer !")
 
 print ("Fin de programme !")
-
